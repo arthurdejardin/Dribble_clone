ef04aa09d280fd816eb9cb9dcbd88006d821cee1
Arthur Dejardin <arthur.dejardin@gmail.com>
2015-03-18 15:09:38 +0000

    add and setup acts as votable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	acts_as_votable
 	has_attached_file :image, styles: { medium: "700x500#", small: "300x250>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
